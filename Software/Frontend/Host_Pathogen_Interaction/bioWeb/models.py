68f4d63640cbd2e4e9a30d21a6192775180f1661
thanujan96 <thanujan96@gmail.com>
2022-04-06 21:39:44 +0530

    feat :  user can add profile picture  fix :  some bugs in login register fixed

--- Software/Frontend/Host_Pathogen_Interaction/bioWeb/models.py
+++ Software/Frontend/Host_Pathogen_Interaction/bioWeb/models.py
@@ -1,11 +1,35 @@
 from fileinput import filename
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import uuid
+from PIL import Image
+from io import BytesIO
+from django.core.files import File
+from django.core.files.base import ContentFile
+
+
+class ResizeImageMixin:
+    def resize(self, imageField: models.ImageField, size: tuple):
+        im = Image.open(imageField)  # Catch original
+        source_image = im.convert("RGB")
+        source_image.thumbnail(size)  # Resize to size
+        output = BytesIO()
+        source_image.save(output, format="JPEG")  # Save resize image to bytes
+        output.seek(0)
+
+        content_file = ContentFile(
+            output.read()
+        )  # Read output and create ContentFile in memory
+        file = File(content_file)
+
+        random_name = f"{uuid.uuid4()}.jpeg"
+        imageField.save(random_name, file, save=False)
+
 
 # Create your models here.
 
 
-class User(AbstractUser):
+class User(AbstractUser, ResizeImageMixin):
     class Meta:
         app_label = "bioWeb"
 
@@ -25,6 +49,12 @@ class User(AbstractUser):
     first_name = models.CharField(max_length=10)
     last_name = models.CharField(max_length=10)
     address = models.CharField(max_length=100, blank=True)
+
+    def save(self, *args, **kwargs):
+        if self.pk is None:
+            self.resize(self.thumb, (200, 200))
+
+        super().save(*args, **kwargs)
 
 
 class Collection(models.Model):
